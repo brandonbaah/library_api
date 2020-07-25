9184ef5aa29702d3e963a796c636475e122f90fa
Brandon Baah <brandonbaah@gmail.com>
2020-07-25 15:17:09 -0400

    added books

---
+++ booksJson.js
@@ -0,0 +1,50 @@
+db.books.insert([
+{
+	title: 'The Unbanking Of America',
+	genre: 'Politics & Social Sciences',
+	author: 'Lisa Servon',
+	read: false
+},
+{
+	title: 'Remote Work',
+	genre: 'Business & Money',
+	author: 'Will Gant',
+	read: false
+},
+{
+	title: 'The Trouble With Black Boys',
+	genre: 'Social Sciences',
+	author: 'Pedro A. Noguera ',
+	read: false
+},
+{
+	title: 'A Journey into the Center of the Earth',
+	genre: 'Science Fiction',
+	author: 'Jules Verne',
+	read: false
+},
+{
+	title: 'The Dark World',
+	genre: 'Fantasy',
+	author: 'Henry Kuttner',
+	read: false
+},
+{
+	title: 'The Wind in the Willows',
+	genre: 'Fantasy',
+	author: 'Kenneth Grahame',
+	read: false
+},
+{
+	title: 'Life On The Mississippi',
+	genre: 'History',
+	author: 'Mark Twain',
+	read: false
+},
+{
+	title: 'Childhood',
+	genre: 'Biography',
+	author: 'Lev Nikolayevich Tolstoy',
+	read: false
+}
+])
